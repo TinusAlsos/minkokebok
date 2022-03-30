a3029aa8192f8351d198aca8d12d531af0e388d3
@tinusfa <tinusfa@stud.ntnu.no>
2022-03-30 18:56:08 +0200

    Add web folder

---
+++ web/pages/test.js
@@ -0,0 +1,13 @@
+function Test() {
+  return <div>Test</div>;
+}
+
+function Test2() {
+  return <div>Test2</div>;
+}
+
+const Test3 = () => {
+  return <div>Test3</div>;
+};
+
+export default Test3;
